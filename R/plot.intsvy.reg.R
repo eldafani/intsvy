40145e96cbdaf718d7aac4c46add65311f429baa
Daniel Caro <dcarov@gmail.com>
2015-01-27 16:25:01 +0000

    Update plot.intsvy.reg.R
    Added argument to select variables for plot and results sorted alphabetically, R-squared at the end

--- R/plot.intsvy.reg.R
+++ R/plot.intsvy.reg.R
@@ -1,14 +1,20 @@
-plot.intsvy.reg <- function(x, se=TRUE, sort=FALSE, ...) {
+plot.intsvy.reg <- function(x, vars, se=TRUE, sort=FALSE, ...) {
+  x= gen.mod
+
+  if (missing(vars)) {
+    vars = row.names(x[[1]])
+  } 
+  
   # it is assumed that last three columns are:  "Freq"       "Mean" "Std.err."  
-  coefE <- lapply(x, function(x) x$Estimate)
-  coefS <- lapply(x, function(x) x$`Std. Error`)
+  coefE <- lapply(gen.mod, function(x) x[row.names(x) %in% vars, "Estimate"])
+  coefS <- lapply(gen.mod, function(x) x[row.names(x) %in% vars, "Std. Error"])
   inds <- which(!is.na(coefE))
   
   estimates <- t(simplify2array(coefE[inds]))
-  colnames(estimates) <- rownames(x[[inds[1]]])
+  colnames(estimates) <- vars
   
   sErrors <- t(simplify2array(coefS[inds]))
-  colnames(sErrors) <- rownames(x[[inds[1]]])
+  colnames(sErrors) <- vars
   
   if (isTRUE(sort)) {
     ord <- order(estimates[,ncol(estimates)])
@@ -17,20 +23,22 @@ plot.intsvy.reg <- function(x, se=TRUE, sort=FALSE, ...) {
   }
   
   ndf <- data.frame(melt(estimates), melt(sErrors)[,3])
-  colnames(ndf) <- c("group", "coefficient", "value", "se")
-  ndf$valueL <- ndf$value - ndf$se
-  ndf$valueH <- ndf$value + ndf$se
+  colnames(ndf) <- c("Group", "Coefficient", "Value", "se")
+  ndf$valueL <- ndf$Value - ndf$se
+  ndf$valueH <- ndf$Value + ndf$se
   
-  pl <- ggplot(data=ndf, aes_string(x = "value", y="group", shape= "coefficient", color="coefficient")) + 
+  # To change order of variable labels in facet_wrap (R-squared appears always in the end)
+  ndf$Coefficient <- factor(ndf$Coefficient, levels = vars)
+  
+  pl <- ggplot(data=ndf, aes_string(x = "Value", y="Group", shape= "Coefficient", color="Coefficient")) + 
     geom_point(size=5) + 
     theme_bw() + 
-    facet_wrap(~coefficient,  scales="free_x") + 
+    facet_wrap(~Coefficient,  scales="free_x") + 
     theme(legend.position="top") 
-    
+  
   if (se) {
     pl <- pl + geom_errorbarh(aes_string(xmin="valueL", xmax="valueH"), width=.5) 
   }
   
   pl
 }
-
