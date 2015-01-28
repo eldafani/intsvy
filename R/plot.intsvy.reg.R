b05f2ff991496f5e12858320fb859a905b56ba67
Daniel Caro <dcarov@gmail.com>
2015-01-28 11:51:19 +0000

    Update plot.intsvy.reg.R

--- R/plot.intsvy.reg.R
+++ R/plot.intsvy.reg.R
@@ -1,42 +1,33 @@
-plot.intsvy.reg <- function(x, vars, se=TRUE, sort=FALSE, ...) {
+plot.intsvy.reg <- function(x, vars, se=TRUE, sort=FALSE) {
   if (missing(vars)) {
     vars = row.names(x[[1]])
-  } 
-  
-  # it is assumed that last three columns are:  "Freq"       "Mean" "Std.err."  
+  }
+  # it is assumed that last three columns are: "Freq" "Mean" "Std.err."
   coefE <- lapply(gen.mod, function(x) x[row.names(x) %in% vars, "Estimate"])
   coefS <- lapply(gen.mod, function(x) x[row.names(x) %in% vars, "Std. Error"])
   inds <- which(!is.na(coefE))
-  
   estimates <- t(simplify2array(coefE[inds]))
   colnames(estimates) <- vars
-  
   sErrors <- t(simplify2array(coefS[inds]))
   colnames(sErrors) <- vars
-  
   if (isTRUE(sort)) {
     ord <- order(estimates[,ncol(estimates)])
     estimates <- estimates[ord,]
     sErrors <- sErrors[ord,]
   }
-  
   ndf <- data.frame(melt(estimates), melt(sErrors)[,3])
   colnames(ndf) <- c("Group", "Coefficient", "Value", "se")
   ndf$valueL <- ndf$Value - ndf$se
   ndf$valueH <- ndf$Value + ndf$se
-  
   # To change order of variable labels in facet_wrap (R-squared appears always in the end)
   ndf$Coefficient <- factor(ndf$Coefficient, levels = vars)
-  
-  pl <- ggplot(data=ndf, aes_string(x = "Value", y="Group", shape= "Coefficient", color="Coefficient")) + 
-    geom_point(size=5) + 
-    theme_bw() + 
-    facet_wrap(~Coefficient,  scales="free_x") + 
-    theme(legend.position="top") 
-  
+  pl <- ggplot(data=ndf, aes_string(x = "Value", y="Group", shape= "Coefficient", color="Coefficient")) +
+    geom_point(size=5) +
+    theme_bw() +
+    facet_wrap(~Coefficient, scales="free_x") +
+    theme(legend.position="top")
   if (se) {
-    pl <- pl + geom_errorbarh(aes_string(xmin="valueL", xmax="valueH"), width=.5) 
+    pl <- pl + geom_errorbarh(aes_string(xmin="valueL", xmax="valueH"), width=.5)
   }
-  
   pl
 }
