977d7e52fad6df989afbf19c65ee68bd6870a24c
pbiecek <przemyslaw.biecek@gmail.com>
2015-10-27 13:59:41 +0100

    generic functions have to be consistent with the default signature

--- R/my.methods.R
+++ R/my.methods.R
@@ -1,6 +1,6 @@
 ### Print outcome
 
-print.intsvy.reg <- function(x) {
+print.intsvy.reg <- function(x, ...) {
   if (all(unlist(lapply(x, is.list)))) { 
     out <- lapply(x, function(y) round(y$reg, 2))
   } else {
@@ -11,11 +11,11 @@ print.intsvy.reg <- function(x) {
 
 ### Summary function
 
-summary.intsvy.reg <- function(x) {
-  if (all(unlist(lapply(x, is.list)))) {
-    out <- lapply(x, function(y) y$reg)
+summary.intsvy.reg <- function(object, ...) {
+  if (all(unlist(lapply(object, is.list)))) {
+    out <- lapply(object, function(y) y$reg)
   } else {
-    out <- x$reg  
+    out <- object$reg  
   }
   return(out)
 }
