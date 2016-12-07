f4a1660b1d871709d3c98019bc3b0c11d098a2fd
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-12-07 18:27:25 +0100

    pv logistic regression for 2015

--- R/pisa.log.pv.R
+++ R/pisa.log.pv.R
@@ -6,3 +6,13 @@ pisa.log.pv <-
                 name= name, folder=folder, config=pisa_conf)
   
 }
+
+pisa2015.log.pv <- 
+  function(pvlabel="READ", x, by, cutoff, data, export=FALSE, 
+           name= "output", folder=getwd()) {
+    
+    intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+                  name= name, folder=folder, config=pisa2015_conf)
+    
+}
+
