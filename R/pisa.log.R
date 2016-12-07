387cfc5391a610c6d6b3a8cc0d5123bb72c00f76
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-12-07 11:10:10 +0100

    logistic regression for pisa 2015

--- R/pisa.log.R
+++ R/pisa.log.R
@@ -4,3 +4,10 @@ pisa.log <-
     intsvy.log(x=x, y=y, by=by, data=data, export=export,
                name=name, folder=folder, config=pisa_conf)
 }
+
+pisa2015.log <- 
+  function(y, x, by, data, export=FALSE, name= "output", folder=getwd()) {
+    
+    intsvy.log(x=x, y=y, by=by, data=data, export=export,
+               name=name, folder=folder, config=pisa2015_conf)
+  }
