787b4e27e75cfbf41538a67f7b30e3845e65162b
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-12-07 10:49:30 +0100

    pisa mean for 2015

--- R/pisa.mean.R
+++ R/pisa.mean.R
@@ -1,8 +1,16 @@
 pisa.mean <- 
-function(variable, by, data, export=FALSE, name= "output", folder=getwd()) {
-  
-  intsvy.mean(variable=variable, by=by, data=data, 
-              export=export, name=name, folder=folder,
-              config=pisa_conf)
-}
+  function(variable, by, data, export=FALSE, name= "output", folder=getwd()) {
+    
+    intsvy.mean(variable=variable, by=by, data=data, 
+                export=export, name=name, folder=folder,
+                config=pisa_conf)
+  }
+
+pisa2015.mean <- 
+  function(variable, by, data, export=FALSE, name= "output", folder=getwd()) {
+    
+    intsvy.mean(variable=variable, by=by, data=data, 
+                export=export, name=name, folder=folder,
+                config=pisa2015_conf)
+  }
 
