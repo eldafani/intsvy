feaa077626afbb61aeee3d48ddbdd72abb68a349
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-12-07 19:40:06 +0100

    all pisa functions get *2015 version

--- R/pisa.ben.pv.R
+++ R/pisa.ben.pv.R
@@ -5,3 +5,11 @@ function(pvlabel, by, cutoff, data, export=FALSE, name= "output", folder=getwd()
                 config=pisa_conf)
 
 }
+
+pisa2015.ben.pv <- 
+  function(pvlabel, by, cutoff, data, export=FALSE, name= "output", folder=getwd()) {
+    
+    intsvy.ben.pv(pvlabel=pvlabel, by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
+                  config=pisa2015_conf)
+    
+  }
