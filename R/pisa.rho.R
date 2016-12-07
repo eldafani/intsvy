7dbf574e9f6ce531fa3c591d06829dbfca10a666
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-12-07 11:01:55 +0100

    pisa 2015 for rho coef

--- R/pisa.rho.R
+++ R/pisa.rho.R
@@ -5,3 +5,11 @@ pisa.rho <-
                folder=folder, config = pisa_conf)
 
 }
+
+pisa2015.rho <-
+function(variables, by, data, export=FALSE, name= "output", folder=getwd()) {
+  
+  intsvy.rho(variables=variables, by=by, data=data, export=export, name=name,
+             folder=folder, config = pisa2015_conf)
+  
+}
