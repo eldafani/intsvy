2e3dd8184f638e70fb48ecbdd7d07506b62bd64a
Jorge Cimentada <cimentadaj@gmail.com>
2016-12-08 16:34:24 +0100

    Update intsvy.select.merge.R

--- R/intsvy.select.merge.R
+++ R/intsvy.select.merge.R
@@ -55,6 +55,9 @@ function(folder=getwd(), countries, student=c(), home, school, teacher, use.labe
   files.select <- lapply(files.all, function(y) sapply(countries, function(x) y[substr(y, 
   nchar(y)+config$input$cnt_part[1], nchar(y)+config$input$cnt_part[2])==tolower(x)])) 
   # no blanks, no home instrument, otherwise delete, see 4g function
+           
+  # Whenever file.select has countries with not directories, filter them out.
+  files.select <- sapply(files.all, function(x) Filter(function(var) length(var) != 0, x))  
   
   # Remove cases for no home instruments
   # only if home is specified
