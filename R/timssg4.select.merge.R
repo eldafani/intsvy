6b90566dd1708cddd9ce801f67a090ab5a936a49
Daniel Caro <dcarov@gmail.com>
2017-04-24 21:47:39 +0100

    Update timssg4.select.merge.R

--- R/timssg4.select.merge.R
+++ R/timssg4.select.merge.R
@@ -1,5 +1,5 @@
 timssg4.select.merge <-
-function(folder=getwd(), countries, student=c(), home, school, teacher, use.labels=TRUE) {
+function(folder=getwd(), countries, student=c(), home, school, teacher, use.labels=FALSE) {
   intsvy.select.merge(folder=folder, countries=countries, student=student, home=home, school=school, teacher = teacher, use.labels=use.labels,
                       config=timss4_conf)
   
