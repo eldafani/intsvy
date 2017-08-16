1b6d230c7951c0a9f03dd13febbe59f1f20ca5b0
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-08-16 17:52:11 +0200

    A new argument `centered=TRUE|FALSE` for `plot.intsvy.table`. Now intsvy tables can be plotted in a similar way as likert plots.

--- R/plot.intsvy.table.R
+++ R/plot.intsvy.table.R
@@ -1,4 +1,4 @@
-plot.intsvy.table <- function(x, se=FALSE, stacked=FALSE, ...) {
+plot.intsvy.table <- function(x, se=FALSE, stacked = FALSE, centered = FALSE, midpoint = NA, ...) {
   # it is assumed that last three columns are:  "Freq"       "Percentage" "Std.err."  
   vars <- setdiff(colnames(x), c("Freq", "Percentage", "Std.err."))
   colnames(x)[ncol(x)] = "se"
@@ -6,43 +6,75 @@ plot.intsvy.table <- function(x, se=FALSE, stacked=FALSE, ...) {
   x$PercentageL <- x$Percentage - 1.96*x$se
   x$PercentageH <- x$Percentage + 1.96*x$se
   pl <- NA
-  if (stacked) {
+  # likert style plots
+  if (centered) {
+    CumSumPercentage <- CumSumPercentageLow <- NULL
+    # make sure that the data.frame x is sorted
+    tmp <- apply(x[,vars[-nvar], drop = FALSE], 1, paste, collapse = "")
+    if (nvar > 1) {
+      x <- x[order(tmp),]
+      tmp <- tmp[order(tmp)]
+    }
+    # calculate midpoint if not specified
+    if (is.na(midpoint)) {
+      midpoint <- (length(unique(x[,vars[nvar]]))+1) / 2
+    }
+    # calculate centered cumsums
+    if (midpoint %% 1 == 0) {
+      # even number of levels
+      x$CumSumPercentage <- unlist(tapply(x$Percentage, tmp, function(y) {
+        cumsum(y) - sum(y[(0:round(midpoint-0.5))[-1]]) - y[round(midpoint+0.5)]/2
+      }))
+    } else {
+      # odd number of levels
+      x$CumSumPercentage <- unlist(tapply(x$Percentage, tmp, function(y) {
+        cumsum(y) - sum(y[(0:midpoint)[-1]])
+      }))
+    }
+    x$CumSumPercentageLow <- x$CumSumPercentage - x$Percentage
+    
+    if (nvar == 1) {
+      pl <- ggplot(data=x, aes(y = CumSumPercentage, ymax = CumSumPercentage, ymin = CumSumPercentageLow, x="variable")) + 
+        geom_crossbar(aes_string(fill = vars[1]), size=0) + ylab("Percentage")
+    } else {
+      if (nvar == 2) {
+        pl <- ggplot(data=x, aes_string(y = "CumSumPercentage", ymax = "CumSumPercentage", ymin = "CumSumPercentageLow", x=vars[1], fill=vars[2])) + 
+          geom_crossbar(size=0)  + ylab("Percentage")
+      } else {
+        pl <- ggplot(data=x, aes_string(y = "CumSumPercentage", ymax = "CumSumPercentage", ymin = "CumSumPercentageLow", x=vars[1], fill=vars[nvar])) + 
+          geom_crossbar(size=0)  +  
+          facet_wrap(as.formula(paste0("~", vars[2]))) + ylab("Percentage")
+      }
+    } 
+  }
+  if (stacked & !centered) {
     if (nvar == 1) {
       pl <- ggplot(data=x, aes_string(y = "Percentage", x=vars[1])) + 
-        geom_bar(stat="identity", position="fill") + theme_bw() +
-        coord_flip() + theme(legend.position="top")
+        geom_bar(stat="identity", position="fill") 
     } else {
       if (nvar == 2) {
         pl <- ggplot(data=x, aes_string(y = "Percentage", x=vars[1], fill=vars[2])) + 
-          geom_bar(stat="identity", position="fill") + 
-          theme_bw() + coord_flip() + theme(legend.position="top")
+          geom_bar(stat="identity", position="fill")       
       } else {
         pl <- ggplot(data=x, aes_string(y = "Percentage", x=vars[1], fill=vars[3])) + 
           geom_bar(stat="identity", position="stack") +  
-          theme_bw() + coord_flip() + 
-          facet_wrap(as.formula(paste0("~", vars[2])))+ 
-          theme(legend.position="top")
+          facet_wrap(as.formula(paste0("~", vars[2])))
       }
     }
-  } else {
+  } 
+  if (!stacked & !centered) {
     if (nvar == 1) {
       pl <- ggplot(data=x, aes_string(y = "Percentage", x=vars[1])) + 
-        geom_bar(stat="identity", position=position_dodge(width = 0.9)) + 
-        theme_bw() + coord_flip() + 
-        theme(legend.position="top")
+        geom_bar(stat="identity", position=position_dodge(width = 0.9)) 
     } else {
       if (nvar == 2) {
         pl <- ggplot(data=x, aes_string(y = "Percentage", x=vars[1], fill=vars[2])) + 
           geom_bar(stat="identity", position=position_dodge(width = 0.9)) + 
-          theme_bw() + coord_flip() + 
-          facet_wrap(as.formula(paste0("~", vars[2])))+ 
-          theme(legend.position="top")
+          facet_wrap(as.formula(paste0("~", vars[2])))
       } else {
         pl <- ggplot(data=x, aes_string(y = "Percentage", x=vars[1], fill=vars[2], col= vars[2])) + 
           geom_bar(stat="identity", position=position_dodge(width = 0.9)) + 
-          theme_bw() + coord_flip() + 
-          facet_wrap(as.formula(paste0("~", vars[3])))+ 
-          theme(legend.position="top")
+          facet_wrap(as.formula(paste0("~", vars[3])))
       }
     }
     
@@ -51,5 +83,7 @@ plot.intsvy.table <- function(x, se=FALSE, stacked=FALSE, ...) {
                                position=position_dodge(width = 0.9), width=.35) 
     } 
   }
+  pl <- pl + theme_bw() + coord_flip() + theme(legend.position="top")
   pl
 }
+  
