5b17fb00b06f57111ae3687e70ab4450c00cced6
eldafani <dcarov@gmail.com>
2018-09-20 10:35:48 +0100

    for cran

--- R/timss.per.pv.R
+++ R/timss.per.pv.R
@@ -5,3 +5,12 @@ timss.per.pv <-
                   name= name, folder=folder, config=timss8_conf)
 
 }
+
+
+timss2015.per.pv <- 
+  function(pvlabel="BSMMAT", by, per, data, export=FALSE, name= "output", folder=getwd()) {
+    
+    intsvy.per.pv(pvlabel=pvlabel, by=by, per=per, data=data, export=export, 
+                  name= name, folder=folder, config=timss2015_conf)
+    
+  }
