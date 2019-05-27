45b7fd63b59e1bf57709796ba52e4ec2939e3a73
Daniel Caro <dcarov@gmail.com>
2019-05-27 17:36:37 +0200

    Update timss.per.pv.R

--- R/timss.per.pv.R
+++ R/timss.per.pv.R
@@ -1,7 +1,7 @@
 timss.per.pv <- 
   function(pvlabel="BSMMAT", by, per, data, export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.per.pv(pvlabel=pvlabel, by=by, per=per, data=data, export=export, 
+    intsvy.per.pv(pvnames = paste(pvlabel, "0", 1:5, sep=""), by=by, per=per, data=data, export=export, 
                   name= name, folder=folder, config=timss8_conf)
 
 }
@@ -10,7 +10,7 @@ timss.per.pv <-
 timss2015.per.pv <- 
   function(pvlabel="BSMMAT", by, per, data, export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.per.pv(pvlabel=pvlabel, by=by, per=per, data=data, export=export, 
+    intsvy.per.pv(pvnames = paste(pvlabel, "0", 1:5, sep=""), by=by, per=per, data=data, export=export, 
                   name= name, folder=folder, config=timss2015_conf)
     
   }
