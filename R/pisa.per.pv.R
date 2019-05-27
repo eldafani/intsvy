3bb74ac18feb6c0b128a3c42358df4dc527a9bb6
Daniel Caro <dcarov@gmail.com>
2019-05-27 17:32:44 +0200

    Update pisa.per.pv.R

--- R/pisa.per.pv.R
+++ R/pisa.per.pv.R
@@ -1,13 +1,13 @@
 pisa.per.pv <-  function(pvlabel="READ", by, per, data, export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.per.pv(pvlabel=pvlabel, by=by, per=per, data=data, export=export, 
+    intsvy.per.pv(pvnames = paste("PV", 1:5, pvlabel, sep=""), by=by, per=per, data=data, export=export, 
                   name= name, folder=folder, config=pisa_conf)
     
 }
 
 pisa2015.per.pv <-  function(pvlabel="READ", by, per, data, export=FALSE, name= "output", folder=getwd()) {
   
-  intsvy.per.pv(pvlabel=pvlabel, by=by, per=per, data=data, export=export, 
+  intsvy.per.pv(pvnames = paste("PV", 1:10, pvlabel, sep=""), by=by, per=per, data=data, export=export, 
                 name= name, folder=folder, config=pisa2015_conf)
   
 }
