244c1b42f6727e6265b9636e8eb1a3e9adbd941c
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:11:57 +0200

    Update piaac.ben.pv.R

--- R/piaac.ben.pv.R
+++ R/piaac.ben.pv.R
@@ -1,6 +1,6 @@
 piaac.ben.pv <- 
 function(pvlabel, by, data, cutoff=cutoff, export=FALSE, name= "output", folder=getwd()) {
-  intsvy.ben.pv(pvlabel=pvlabel, by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
+  intsvy.ben.pv(pvnames = paste("PV", pvlabel, 1:10, sep=""), by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
                 config=piaac_conf)
 
   }
