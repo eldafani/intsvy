e4c9940a0b925718b6f45f9eef6c567fa38ca3db
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:16:41 +0200

    Update timss.log.pv.R

--- R/timss.log.pv.R
+++ R/timss.log.pv.R
@@ -2,7 +2,7 @@ timss.log.pv <-
   function(pvlabel="BSMMAT", x, by, cutoff, data, 
            export=FALSE, name= "output", folder=getwd()) {
     
-  intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+  intsvy.log.pv(x=x, pvnames = paste(pvlabel, "0", 1:5, sep=""), cutoff=cutoff, by=by, data=data, export=export, 
                 name= name, folder=folder, config=timss8_conf)
   
   }
@@ -12,7 +12,7 @@ timss2015.log.pv <-
   function(pvlabel="BSMMAT", x, by, cutoff, data, 
            export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+    intsvy.log.pv(x=x, pvnames = paste(pvlabel, "0", 1:5, sep=""), cutoff=cutoff, by=by, data=data, export=export, 
                   name= name, folder=folder, config=timss2015_conf)
     
   }
