64610fb30bcc6321a41abc3c7e0a8e2e3f059bed
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:06:27 +0200

    Update pisa.ben.pv.R

--- R/pisa.ben.pv.R
+++ R/pisa.ben.pv.R
@@ -1,7 +1,7 @@
 pisa.ben.pv <- 
 function(pvlabel, by, cutoff, data, export=FALSE, name= "output", folder=getwd()) {
   
-  intsvy.ben.pv(pvlabel=pvlabel, by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
+  intsvy.ben.pv(pvnames = paste("PV", 1:5, pvlabel, sep=""), by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
                 config=pisa_conf)
 
 }
@@ -9,7 +9,7 @@ function(pvlabel, by, cutoff, data, export=FALSE, name= "output", folder=getwd()
 pisa2015.ben.pv <- 
   function(pvlabel, by, cutoff, data, export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.ben.pv(pvlabel=pvlabel, by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
+    intsvy.ben.pv(pvnames = paste("PV", 1:10, pvlabel, sep=""), by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
                   config=pisa2015_conf)
     
   }
