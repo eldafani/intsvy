4f0d5bfbdff292920c3c0e7fa9d2cfd337a9ece5
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:01:50 +0200

    Update timss.reg.pv.R

--- R/timss.reg.pv.R
+++ R/timss.reg.pv.R
@@ -1,7 +1,7 @@
 timss.reg.pv <-
   function(x, pvlabel="BSMMAT",  by, data, std=FALSE, export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.reg.pv(x=x, pvlabel=pvlabel, by=by, data=data, std=std, export=export, 
+    intsvy.reg.pv(x=x, pvnames = paste(pvlabel, "0", 1:5, sep=""), by=by, data=data, std=std, export=export, 
                   name= name, folder=folder, config=timss8_conf) 
     
 }
@@ -9,6 +9,6 @@ timss.reg.pv <-
 timss2015.reg.pv <-
   function(x, pvlabel="BSMMAT",  by, data, std=FALSE, export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.reg.pv(x=x, pvlabel=pvlabel, by=by, data=data, std=std, export=export, 
+    intsvy.reg.pv(x=x, pvnames = paste(pvlabel, "0", 1:5, sep=""), by=by, data=data, std=std, export=export, 
                   name= name, folder=folder, config=timss2015_conf) 
 }
