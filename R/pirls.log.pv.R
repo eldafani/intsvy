3af7493a6bb4b9ee2bf6b09cceee747df2c2ace7
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:16:00 +0200

    Update pirls.log.pv.R

--- R/pirls.log.pv.R
+++ R/pirls.log.pv.R
@@ -2,7 +2,7 @@ pirls.log.pv <-
   function(pvlabel="ASRREA", x, cutoff, by, data, 
            export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+    intsvy.log.pv(x=x, pvnames = paste(pvlabel, "0", 1:5, sep=""), cutoff=cutoff, by=by, data=data, export=export, 
                   name= name, folder=folder, config=pirls_conf)
         
   }
@@ -11,7 +11,7 @@ pirls2016.log.pv <-
   function(pvlabel="ASRREA", x, cutoff, by, data, 
            export=FALSE, name= "output", folder=getwd()) {
     
-    intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+    intsvy.log.pv(x=x, pvnames = paste(pvlabel, "0", 1:5, sep=""), cutoff=cutoff, by=by, data=data, export=export, 
                   name= name, folder=folder, config=pirls2016_conf)
     
   }
