fbe080597baa1826787c1c9406e9a7fdd95ee392
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:13:18 +0200

    Update pisa.log.pv.R

--- R/pisa.log.pv.R
+++ R/pisa.log.pv.R
@@ -2,7 +2,7 @@ pisa.log.pv <-
   function(pvlabel="READ", x, by, cutoff, data, export=FALSE, 
            name= "output", folder=getwd()) {
     
-  intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+  intsvy.log.pv(x=x, pvnames = paste("PV", 1:5, pvlabel, sep=""), cutoff=cutoff, by=by, data=data, export=export, 
                 name= name, folder=folder, config=pisa_conf)
   
 }
@@ -11,7 +11,7 @@ pisa2015.log.pv <-
   function(pvlabel="READ", x, by, cutoff, data, export=FALSE, 
            name= "output", folder=getwd()) {
     
-    intsvy.log.pv(x=x, pvlabel=pvlabel, cutoff=cutoff, by=by, data=data, export=export, 
+    intsvy.log.pv(x=x, pvnames = paste("PV", 1:10, pvlabel, sep=""), cutoff=cutoff, by=by, data=data, export=export, 
                   name= name, folder=folder, config=pisa2015_conf)
     
 }
