4e4dd5e23e919167460a4547224f33d8c364073e
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:20:12 +0200

    Update pirls.rho.pv.R

--- R/pirls.rho.pv.R
+++ R/pirls.rho.pv.R
@@ -1,7 +1,7 @@
 pirls.rho.pv <-
 function(variable, pvlabels, by, data, export=FALSE, name= "output", folder=getwd()) {
   
-  intsvy.rho.pv(variable=variable, pvlabels=pvlabels, 
+  intsvy.rho.pv(variable=variable, pvnames = paste(pvlabel, "0", 1:5, sep=""), 
                 by=by, data=data, export=export, 
                 name= name, folder=folder, config = pirls_conf)
 }
