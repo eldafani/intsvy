83ad68fc1cdb7ee1bd145e86ec422152641bbc08
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:11:14 +0200

    Update timss.ben.pv.R

--- R/timss.ben.pv.R
+++ R/timss.ben.pv.R
@@ -1,13 +1,13 @@
 timss.ben.pv <- function(pvlabel, by, cutoff=cutoff, data, export=FALSE, name= "output", folder=getwd()) {
   
-  intsvy.ben.pv(pvlabel=pvlabel, by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
+  intsvy.ben.pv(pvnames = paste(pvlabel, "0", 1:5, sep=""), by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
                 config=timss8_conf)
     
 }
 
 timss2015.ben.pv <- function(pvlabel, by, cutoff=cutoff, data, export=FALSE, name= "output", folder=getwd()) {
   
-  intsvy.ben.pv(pvlabel=pvlabel, by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
+  intsvy.ben.pv(pvnames = paste(pvlabel, "0", 1:5, sep=""), by=by, cutoff=cutoff, data=data, export=export, name= name, folder=folder,
                 config=timss2015_conf)
   
 }
