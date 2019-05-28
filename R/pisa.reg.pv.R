b210409601f98f60302330f7dde3e153fdc940be
Daniel Caro <dcarov@gmail.com>
2019-05-28 11:52:25 +0200

    Update pisa.reg.pv.R

--- R/pisa.reg.pv.R
+++ R/pisa.reg.pv.R
@@ -1,13 +1,13 @@
 pisa.reg.pv <- 
   function(x, pvlabel="READ", by, data, export=FALSE, name= "output", folder=getwd(), std=FALSE) {
     
-    intsvy.reg.pv(x=x, pvlabel=pvlabel, by=by, data=data, std=std, export=export, 
+    intsvy.reg.pv(x=x, pvnames = paste("PV", 1:5, pvlabel, sep=""), by=by, data=data, std=std, export=export, 
                   name= name, folder=folder, config=pisa_conf) 
 }
 
 pisa2015.reg.pv <- 
   function(x, pvlabel="READ", by, data, export=FALSE, name= "output", folder=getwd(), std=FALSE) {
     
-    intsvy.reg.pv(x=x, pvlabel=pvlabel, by=by, data=data, std=std, export=export, 
+    intsvy.reg.pv(x=x, pvnames = paste("PV", 1:10, pvlabel, sep=""), by=by, data=data, std=std, export=export, 
                   name= name, folder=folder, config=pisa2015_conf) 
   }
