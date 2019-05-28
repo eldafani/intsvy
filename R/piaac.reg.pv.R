29b66bff8dce7fd3ffea64f5083633d27a241e72
Daniel Caro <dcarov@gmail.com>
2019-05-28 12:02:44 +0200

    Update piaac.reg.pv.R

--- R/piaac.reg.pv.R
+++ R/piaac.reg.pv.R
@@ -1,7 +1,7 @@
 piaac.reg.pv <- 
 function(x, pvlabel="LIT", by, data, export=FALSE, name= "output", std=FALSE, folder=getwd()) {
 
-  intsvy.reg.pv(x=x, pvlabel=pvlabel, by=by, data=data, std=std, export=export, 
+  intsvy.reg.pv(x=x, pvnames = paste("PV", pvlabel, 1:10, sep=""), by=by, data=data, std=std, export=export, 
                 name= name, folder=folder, config=piaac_conf) 
   
 }
