b48ef9882a59192919882593ff48dd19db13a67e
Daniel Caro <dcarov@gmail.com>
2021-08-20 10:40:23 +0200

    Update piaac.mean.pv.R

--- R/piaac.mean.pv.R
+++ R/piaac.mean.pv.R
@@ -1,7 +1,7 @@
 piaac.mean.pv <- 
   function(pvlabel, by, data, export=FALSE, name= "output", folder=getwd()) {
 
-    intsvy.mean.pv(pvnames = paste("PV", pvlabel, 1:10, sep=""), 
+    intsvy.mean.pv(pvnames = pvlabel, 
                    by=by, data=data, export=export,
                    name=name, folder=folder, config=piaac_conf)
 
