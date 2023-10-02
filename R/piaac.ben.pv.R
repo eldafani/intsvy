4e5908df394ff26671bc5f1f8a472ad8735618be
eldafani <dcarov@gmail.com>
2023-10-02 20:16:27 +0200

    fix format

--- R/piaac.ben.pv.R
+++ R/piaac.ben.pv.R
@@ -1,7 +1,7 @@
 piaac.ben.pv <- 
 function(pvlabel, by, data, cutoff=cutoff, atlevel=FALSE, export=FALSE, 
          name= "output", folder=getwd()) {
-  intsvy.ben.pv(pvnames, by=by, cutoff=cutoff, data=data, 
+  intsvy.ben.pv(pvnames=pvlabel, by=by, cutoff=cutoff, data=data, 
                 atlevel=atlevel, export=export, name= name, folder=folder,
                 config=piaac_conf)
 
--- R/piaac.reg.pv.R
+++ R/piaac.reg.pv.R
@@ -1,7 +1,7 @@
 piaac.reg.pv <- 
 function(x, pvlabel, by, data, export=FALSE, name= "output", std=FALSE, folder=getwd()) {
 
-  intsvy.reg.pv(x=x, pvnames, by=by, data=data, std=std, export=export, 
+  intsvy.reg.pv(x=x, pvnames=pvlabel, by=by, data=data, std=std, export=export, 
                 name= name, folder=folder, config=piaac_conf) 
   
 }
